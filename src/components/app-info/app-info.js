76e18b795e1bccdcf3695138b4860213a0fbb579
Bongarev <shtefan.1035@gmail.com>
2023-04-27 18:58:22 +0300

    react project

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+  return (
+    <div className="app-info">
+      <h1>Workers Record</h1>
+      <h2>Total number of workers:</h2>
+      <h2>Award-Winning worker(s):</h2>
+    </div>
+  )
+}
+
+export default AppInfo;
